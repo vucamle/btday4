57b3d8ff8c00f0a16d7849113adf1b7d92cf5fdc
vucamle <vuptbd00080@fpt.edu.vn>
2022-11-20 23:07:40 +0700

    index.html

--- day4.js
+++ day4.js
@@ -1,2 +1,16 @@
-let a = '45';
-console.log(Number.isNaN(a));
+ //bt1
+ 
+ let a = '12';
+ console.log(Number.isFinite(a) && Number.isNaN(a));
+
+
+
+
+//bt3
+let ages = [3,3,4];
+
+function checkAge(age) {
+  return age != 3;
+}
+let age = ages.find(checkAge);
+console.log(age); 
